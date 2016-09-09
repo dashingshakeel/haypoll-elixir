7ac95b7804d75488a6baea10fa3df1744d37bbb8
dashingshakeel <shkeelrauf@gmail.com>
2016-09-09 22:55:18 +0500

    Added entry in form and added js for poll

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,8 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import  {Poll} from "./poll"
+let poll = new Poll()
 
 // Import local files
 //
